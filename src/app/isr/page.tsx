08258c7b56aac7c7c58559f0f87a9760b836534c
gkstnadl <gkstnadl12@naver.com>
2024-03-11 20:56:18 +0900

    Feat: next.js 첫 과제, 뼈대 만들기

---
+++ src/app/isr/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>isr페이지 입니다.</div>;
+};
+
+export default page;
---
+++ src/app/ssr/ssr.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>ssr페이지 입니다.</div>;
+};
+
+export default page;
